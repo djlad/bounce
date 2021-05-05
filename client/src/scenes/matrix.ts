01d14a8828f2ca64e6eb40007b86cd4c43502c6c
daniel <daniel.ladner@ecs.global>
2021-05-05 19:29:12 -0400

    fixed sprites

--- client/src/scenes/matrix.ts
+++ client/src/scenes/matrix.ts
@@ -1,18 +1,63 @@
+import { Vector } from "matter";
 import "phaser"
+import { Physics } from "phaser";
 
 export class MatrixScene extends Phaser.Scene
 {
-  preload() {
-    console.log("preloading");
-    this.load.spritesheet("slime", "assets/slime.jiggle.png", {frameWidth: 64, frameHeight: 64});
-    this.load.image("ball", "assets/ball.jpeg");
+  cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+  player?: Physics.Matter.Sprite;
+
+  init()
+  {
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
+
+  createBall(radius : number, spriteKey: string) : Phaser.Physics.Matter.Sprite {
+    let ball = this.matter.add.sprite(0, 0, spriteKey);
+    ball.displayWidth = radius * 2;
+    ball.displayHeight = radius * 2;
+    ball.setCircle(radius);
+    return ball;
+  }
+
+  handleMove() : void {
+    if (this.cursors?.left.isDown)
+    {
+      this.player?.setVelocityX(-5);
+      // this.player.flipX = false;
+    }
+    if (this.cursors?.right.isDown)
+    {
+      this.player?.setVelocityX(5);
+    }
+    this.cursors?.up.on("up", (e: any) => {
+      this.player?.setVelocityY(-5);
+      // this.player?.applyForce(new Phaser.Math.Vector2(0, -.000001))
+    })
+  }
+  preload() {
+    this.load.spritesheet("slime", "assets/slime.jiggle.png", {frameWidth: 64, frameHeight: 64});
+    this.load.image("ball", "assets/ball.png");
+  }
+
   create() {
     this.cameras.main.setBackgroundColor("blue");
-    let player = this.matter.add.sprite(100, 100, "slime");
+    this.player = this.matter.add.sprite(100,100, "slime");
     let floor = this.matter.add.rectangle(0, 500, 2000, 20);
-    floor.gravityScale.y = 0;
     floor.isStatic = true;
-    this.matter.add.sprite(200,100, "ball");
+    let ball = this.createBall(30, "ball");
+    ball.setX(300);
+    ball.setY(100);
+    this.anims.create({
+      key: 'slimewalk',
+      frames: this.anims.generateFrameNumbers('slime', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.player.play("slimewalk");
+  }
+
+  update() {
+   this.handleMove();
   }
 }
